481f64ed325463577d17e5545feee764f26dd9a4
yuliakrysova <kryuma2oos@gmail.com>
2024-11-02 18:26:54 +0300

    добавление функции возвращения данных в виде кортежа с помощью ключевого слова yield для создания генератора

--- parsing_website1.py
+++ parsing_website1.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
+
 headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0'}
 def get_url():
 
@@ -20,22 +21,22 @@ def get_url():
             card_url = "https://scrapingclub.com" + i.find('a').get('href')
             yield card_url
 
+def array():
 
-for card_url in get_url():
+    for card_url in get_url():
 
-    response = requests.get(card_url, headers=headers)
+        response = requests.get(card_url, headers=headers)
 
-    sleep(3)
-    soup = BeautifulSoup(response.text, 'lxml') #lxml парсер кода
+        sleep(3)
+        soup = BeautifulSoup(response.text, 'lxml') #lxml парсер кода
 
-    data = soup.find('div', class_='my-8 w-full rounded border')
-    name = data.find('h3').text
+        data = soup.find('div', class_='my-8 w-full rounded border')
+        name = data.find('h3').text
 
-    price = data.find('h4').text
+        price = data.find('h4').text
 
-    text = data.find('p').text
+        text = data.find('p').text
 
-    url_img = "https://scrapingclub.com" + data.find("img").get("src")
+        url_img = "https://scrapingclub.com" + data.find("img").get("src")
 
-    print(name, price, text, url_img, sep='\n')
-    print()
+        yield name, price, text, url_img
